50577cfc994a4cc4815abf473f9e8b6c6d804d64
lanckrietvictor <lanckrietvictor@gmail.com>
2017-10-04 11:31:05 +0200

    start MVC

---
+++ Controller/router.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'View/header.php';
+
+if (empty($_GET)) {
+  require_once 'View/homepage.php';
+}
+
+require_once 'View/footer.php';
+
+?>
---
+++ View/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="View/CSS/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="View/CSS/style.css">
+    <title>AIP</title>
+</head>
+<body>
+  <div class="container-fluid padding">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 navbar">
+        <div class="row">
+          <div class="col-xs-6 col-sm-6">
+            <img class="img-responsive" src="View/Pics/1_Primary_logo_on_transparent_249x69.png" alt="Logo AIP">
+          </div>
+          <div class="col-xs-1 col-sm-1">
+
+          </div>
+          <div class="col-xs-5 col-sm-5" id="signIn">
+            <i class="fa fa-user-circle" aria-hidden="true"></i>
+            Sign in
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 navbar">
+        <div class="row">
+          <div class="col-xs-3 col-sm-3">
+            
+          </div>
+        </div>
+    </div
+  </div>
